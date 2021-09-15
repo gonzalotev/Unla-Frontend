b2fbd8f1a8f7dd3df3a0803747ff544722e8e858
gonzalo <gonzalotevez222@gmail.com>
2021-09-15 16:55:59 -0300

    reform login

---
+++ src/components/Login/FormLib.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { useField } from "formik";
+import { StyledTextInput, StyledLabel, StyledIcon } from '../styled/Styles';
+import { FiEyeOff, FiEye } from "react-icons/fi";
+
+export const TextInput = ({icon, ...props}) => {
+    const [field, meta] = useField(props);
+    const [show, setShow] = useState(false);
+
+    return(
+        <div style={{position: "relative"}}>
+            <StyledLabel htmlFor={props.name}>
+                {props.label}
+            </StyledLabel>
+            {props.type !== "password" && <StyledTextInput {...field} {...props}
+            />}
+            {props.type === "password" && (
+                <StyledTextInput  {...field} {...props}
+                type={show ? "text" : "password"}
+                />
+            )}
+
+            <StyledIcon>
+                {icon}
+            </StyledIcon>
+            {
+                props.type === "password" &&
+                <StyledIcon onClick={() => setShow(!show)} right>
+                    {show &&<FiEye/>}
+                    {!show && <FiEyeOff/>}
+                </StyledIcon>
+            }
+        </div>
+    )
+}
